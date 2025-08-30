b84931f0d976ea4bf8db4fe9be899b91fafef07c
romalopes <romalopes@yahoo.com.br>
2025-08-31 07:45:34 +1000

    delegate call.

---
+++ contracts/Delegate/DelegateCallExample.sol
@@ -0,0 +1,25 @@
+//  SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract B {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(uint _num) public payable{
+        num = _num;
+        sender = msg.sender;
+        value = msg.value;
+    }
+}
+
+contract A {
+    uint public num;
+    address public sender;
+    uint public value;
+
+    function setVars(address _contract, uint _num) public payable{
+        //A's storage is set, B is not modified
+        (bool successs, bytes memory data) = _contract.delegatecall(abi.encodeWithSignature("setVars(uint", _num));
+    }
+}
